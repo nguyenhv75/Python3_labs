b49eea54322165750a058778d73c0678d40b1db8
Hien Nguyen <nguyenhv75@gmail.com>
2024-11-18 20:49:21 +0000

    Created first.py

---
+++ first.py
@@ -0,0 +1,18 @@
+#! /usr/bn/env python3
+# Author: HNguyen
+# version: 1.0
+# Description: This is my first script.
+
+# name = "Hien Nguyen"
+name = input("Enter your name: ")
+
+print("My name is",name) # comma puts space between parameters.
+print("My name is " + name)
+
+import random
+print("My lucky number is", random.randint(1,50))
+
+import math
+print("Cosline of  0.5", math.cos(0.5))
+
+      
