6c4f8138767e0b88bd30668bcf945405bab9f40e
Hien Nguyen <nguyenhv75@gmail.com>
2024-11-19 19:32:27 +0000

    Updated demo_pin.py

--- demo_pin.py
+++ demo_pin.py
@@ -2,18 +2,26 @@
 # Author: Hien Nguyen
 # Version: 1.0
 # Description: This script will simulate a high street bank PIN machine
+# Max 3 attempts!
 """
 Docstring:
 """
 
 master_pin = "0123"
 pin = None
+tries = 0
 
-while pin != master_pin:
+while pin != master_pin and tries < 3:
     pin = input("Enter PIN: ")
     if pin == master_pin:
         print("Valid PIN")
+        break
     else:
         print("Invalid PIN")
+        tries += 1
+else:
+    # Executes only ONCE when condition becomes False!
+    print("You had", tries, "tries and failed!")
+    print("Your card has been retained. Have a nice day :-)")
 
 print("Done.")
