2566d1b9cf1d9cc293ec73943a75d0131056386e
Hien Nguyen <nguyenhv75@gmail.com>
2024-11-19 19:03:36 +0000

    Updated lotto.py

--- lotto.py
+++ lotto.py
@@ -4,6 +4,16 @@
 # Description: This script will generate 6 random
 # lottery numbers
 """
-
+    Docstring
 """
-print()
+import random
+lotto = [] # Create an empty list.
+
+while len(lotto) < 6:
+    num = random.randint(1,50)
+    if num not in lotto:
+        lotto.append(num)
+    else:
+        print("Duplicate number:", num)
+
+print("Lottery numbers =", lotto)
